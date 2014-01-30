4fb4d999cc9be5d3827be47ec66205f2b7b9ebf2
Lynn Walker <lynn@winmedia.com>
2014-01-30 12:51:09 -0800

    first working version

--- app/js/app.js
+++ app/js/app.js
@@ -1,16 +1,44 @@
 'use strict';
 
+var API_KEY = "dxgknt8n445rbpycq73md34k";
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
+angular.module('movieApp', [
+  'ngRoute'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.when('/search', {templateUrl: 'partials/search.html'});
+  $routeProvider.when('/about', {templateUrl: 'partials/about.html'});
+  $routeProvider.otherwise({redirectTo: '/search'});
 }]);
+
+var SearchCtrl = function($scope, $routeParams, $http, $location) {
+	redirectToBaseUI($location);
+
+	$scope.onSearchClicked = function(moviename) {
+		redirectToBaseUI($location);
+		$scope.movieResult = searchMovie(moviename, $scope, $http);
+	};
+};
+
+function redirectToBaseUI($location) {
+	// set the correct path if it is not.
+	if( $location.path() != "/search" ) {
+		console.log('setting the root path first');
+		$location.path("/search");.0
+	}
+}
+
+function searchMovie(moviename, $scope, $http) {
+	var APIUrl = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=" + API_KEY + "&q=" + moviename + "&callback=JSON_CALLBACK";
+
+	$http({method: 'JSONP', url: APIUrl})
+	.success(function(data, status, headers, config) {
+		$scope.movieResult = data;
+		console.log('Success ');
+		console.log(movieResult);
+	})
+	.error(function(data, status, headers, config) {
+		console.log('Error ' + status);
+	});
+}
